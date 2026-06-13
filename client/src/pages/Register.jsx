0a43e917a8f95e9b1baa618f1a962c3e699f8cc2
MANSI PATIL <patilmansi661@gmail.com>
2026-06-13 14:04:02 +0530

    Frontend ready for deployment

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -22,7 +22,7 @@ function Register() {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/auth/register",
+        "https://educonnect-pro-74gu.onrender.com/api/auth/register",
         formData
       );
 
